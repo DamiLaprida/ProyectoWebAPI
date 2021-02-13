5346d75fe2caa5078f7dca9f0a3cdffb9e0e3d39
Damian <damianfacu18@hotmail.com>
2021-02-13 18:33:01 -0300

    Proyecto básico web api

---
+++ ProyectoWebAPI/Controllers/APIPerfilPersonalController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIPerfilPersonalController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(int id)
+        {
+            //Codigo para leer de la base de datos
+            return id switch
+            {
+                1 => "Ivan",
+                2 => "Curso",
+                _ => throw new NotSupportedException("El id no es válido")
+            };
+        }
+
+        [HttpPost]
+        public string Post(PerfilPersonalDTO perfilPersonal) 
+        {
+            //Guardar perfil en la base de datos
+            return perfilPersonal.Nombre;
+        }
+    }
+
+    public class PerfilPersonalDTO
+    {
+       public string Nombre { get; set; }
+       public string Apellido { get; set; }
+       
+    }
+}
